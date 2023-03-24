80a033e1a81136b26ea35841a2758a502b245498
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-24 19:46:00 +0530

    Add LoginBOImpl class

---
+++ Hostal-Management/src/lk/ijse/hostal_management/bo/custom/impl/LoginBOImpl.java
@@ -0,0 +1,22 @@
+package lk.ijse.hostal_management.bo.custom.impl;
+
+import bo.custom.LoginBO;
+import bo.custom.UserBO;
+import dao.Custom.UserDAO;
+import dao.DAOFactory;
+
+import java.util.HashMap;
+
+public class LoginBOImpl implements LoginBO {
+    private final UserDAO uDAO = (UserDAO) DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.USER);
+
+    @Override
+    public boolean checkCredentials(String userName, String password) throws Exception {
+        HashMap<String, String> allUsers = uDAO.getAllUserNPasswordMap();
+        if (allUsers.containsKey(userName)) {
+            return allUsers.get(userName).equals(password);
+        } else {
+            return false;
+        }
+    }
+}
